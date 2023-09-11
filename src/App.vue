5302f762d3ef710332c12a8e94de73f1a1a792a1
Catiflam <gilfrid1@gmail.com>
2023-09-11 17:51:19 +0200

    edit layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppCard from "./components/AppCard.vue";
+import AppFooter from "./components/AppFooter.vue";
+
 export default {
 	data() {
 		return {
 			title: "hello vue",
 		};
 	},
+
+	components: {
+		AppHeader,
+		AppCard,
+		AppFooter,
+	},
 };
 </script>
 
 <template>
-	<h1>{{ title }}</h1>
+	<AppHeader />
+	<AppCard />
+	<AppFooter />
 </template>
 
 <style lang="scss">
-@use "./assets/styles/general.scss";
+@use "./assets/styles/general.scss" as *;
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,105 @@
+<script>
+export default {
+	data() {
+		return {
+			links: [
+				{
+					text: "Characters",
+					url: "#",
+					active: true,
+				},
+				{
+					text: "Comics",
+					url: "#",
+					active: false,
+				},
+				{
+					text: "Movies",
+					url: "#",
+					active: false,
+				},
+				{
+					text: "Tv",
+					url: "#",
+					active: false,
+				},
+				{
+					text: "Games",
+					url: "#",
+					active: false,
+				},
+				{
+					text: "Collectibles",
+					url: "#",
+					active: false,
+				},
+				{
+					text: "Videos",
+					url: "#",
+					active: false,
+				},
+				{
+					text: "Fans",
+					url: "#",
+					active: false,
+				},
+				{
+					text: "News",
+					url: "#",
+					active: false,
+				},
+
+				{
+					text: "Shop",
+					url: "#",
+					active: false,
+				},
+			],
+		};
+	},
+};
+</script>
+
+<template>
+	<header>
+		<div class="container">
+			<div class="col">
+				<img src="../../public/img/dc-logo.png" alt="" />
+			</div>
+			<ul>
+				<li v-for="link in links"><a :href="link.url" :class="link.active ? 'active' : ''"></a>{{ link.text }}</li>
+			</ul>
+		</div>
+	</header>
+	<main>
+		<div class="wrap">
+			<img src="../../public/img/jumbotron.jpg" alt="" />
+		</div>
+	</main>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/styles/partials/mixins" as *;
+header {
+	@include center();
+	flex-direction: column;
+	padding-bottom: 2rem;
+}
+ul {
+	margin-top: 2rem;
+}
+li {
+	display: inline-block;
+	padding: 1rem;
+	font-weight: bold;
+	cursor: pointer;
+
+	&.active,
+	&:hover {
+		background-color: aqua;
+	}
+}
+.active {
+	background-color: aqua;
+}
+</style>
